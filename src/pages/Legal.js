ef57b6445fc7694632736952348eb3f9b0be16bf
Remi-godji <rgodji16@gmail.com>
2025-03-08 15:35:26 +0100

    commencement de la mention legales

--- src/pages/Legal.js
+++ src/pages/Legal.js
@@ -4,6 +4,9 @@ import { Helmet } from "react-helmet"; // Pour la balise meta noindex
 import Header from "../components/Header"; // Import du Header
 import Footer from "../components/Footer"; // Import du Footer
 import "../styles/legal.css";
+import { faMapMarkedAlt, faMapPin, faPhoneAlt, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons"; // Import de l'icône "Globe"
 
 const Legal = () => {
   return (
@@ -26,31 +29,54 @@ const Legal = () => {
         <Accordion defaultActiveKey="0" className="legal-accordion">
           {/* Section sur l'éditeur du site */}
           <Accordion.Item eventKey="0">
-            <Accordion.Header>L’Éditeur du site</Accordion.Header>
+            <Accordion.Header>Éditeur du site</Accordion.Header>
             <Accordion.Body>
-              Ce site est édité par John Doe, développeur web en formation.  
-              <br />
-              Adresse : 123 rue du Code, 75000 Paris  
-              <br />
-              Email : contact@johndoeportfolio.com  
-              <br />
-              Téléphone : +33 (0)6 12 34 56 78
+
+            <h2>John Doe</h2>
+
+{/* Adresse avec icône carte */}
+<p>
+  <FontAwesomeIcon icon={faMapMarkedAlt} style={{ color: "black", marginRight: "10px" }} />
+  40 rue Laure Diebold
+</p>
+
+{/* Ville avec icône cible */}
+<p>
+  <FontAwesomeIcon icon={faMapPin} style={{ color: "black", marginRight: "10px" }} />
+  69009 Lyon, France
+</p>
+
+{/* Téléphone avec icône téléphone */}
+<p>
+  <FontAwesomeIcon icon={faPhoneAlt} style={{ color: "black", marginRight: "10px" }} />
+  10 20 30 40 50
+</p>
+
+{/* Email avec icône enveloppe */}
+<p>
+  <FontAwesomeIcon icon={faEnvelope} style={{ color: "black", marginRight: "10px" }} />
+  john.doe@gmail.com
+</p>
             </Accordion.Body>
           </Accordion.Item>
 
-          {/* Section sur l'hébergeur du site */}
           <Accordion.Item eventKey="1">
-            <Accordion.Header>L’Hébergeur du site</Accordion.Header>
-            <Accordion.Body>
-              Ce site est hébergé par OVHcloud.  
-              <br />
-              Adresse : 2 rue Kellermann, 59100 Roubaix, France  
-              <br />
-              Téléphone : +33 9 72 10 10 07  
-              <br />
-              Site web : <a href="https://www.ovhcloud.com" target="_blank" rel="noopener noreferrer">www.ovhcloud.com</a>
-            </Accordion.Body>
-          </Accordion.Item>
+  <Accordion.Header>Hébergeur</Accordion.Header>
+  <Accordion.Body>
+    <h2>alwaysdata</h2>
+    <div className="legal-section">
+      91 Rue du Faubourg Saint-Honoré, 75008 Paris
+    </div>
+
+    <div className="legal-section">
+      <a href="https://www.alwaysdata.com" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faGlobe} style={{ color: "black", marginRight: "5px" }} />
+        www.alwaysdata.com
+      </a>
+    </div>
+  </Accordion.Body>
+</Accordion.Item>
+
 
           {/* Section sur les crédits */}
           <Accordion.Item eventKey="2">
@@ -58,14 +84,21 @@ const Legal = () => {
             <Accordion.Body>
               <h2>Crédits</h2>
 
-              Ce site a été réalisé par John Doe, étudiant au&nbsp;
-<a href="https://www.centre-europeen-formation.fr/" target="_blank" rel="noopener noreferrer">Centre Européen de formation</a>.
+              <div class="legal-section">
+  Ce site a été réalisé par John Doe, étudiant au&nbsp;
+  <a href="https://www.centre-europeen-formation.fr/" target="_blank" rel="noopener noreferrer">Centre Européen de formation</a>.
+</div>
 
-              <br />
-            <i>  Les images utilisés sur ce site sont libres de droits et ont été obtenues sur le site&nbsp;
-            <a href="https://www.pixabay.com" target="_blank" rel="noopener noreferrer">Pixabay</a>.  </i>
-            <br />
-              - Le développement et la mise en page de ce site ont été réalisés en React et Bootstrap.
+<div class="legal-section">
+  <i>Les images utilisées sur ce site sont libres de droits et ont été obtenues sur le site&nbsp;
+  <a href="https://www.pixabay.com" target="_blank" rel="noopener noreferrer">Pixabay</a>.</i>
+</div>
+
+<div class="legal-section">
+  <i>Le fav icon de ce site a été fourni par&nbsp;
+  <a href="https://www.flaticon.com/de/kostenlose-icons/john-doe" target="_blank" rel="noopener noreferrer">John Doe Icons créé par Freepik - Flaticon</a>.</i>
+</div>
+
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,53 +1,114 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import banner from "../assets/images/banner.jpg";
+import coder from "../assets/portfolio/coder.jpg";
+import espaceBienEtre from "../assets/portfolio/espace-bien-etre.jpg";
+import freshFood from "../assets/portfolio/fresh-food.jpg";
+import restaurantJaponais from "../assets/portfolio/restaurant-japonais.jpg";
+import screen from "../assets/portfolio/screens.jpg";
+import seo from "../assets/portfolio/seo.jpg";
+import "../styles/portfolio.css";
 
 const Portfolio = () => {
   const projects = [
     {
       title: "Projet 1",
-      description: "Description du projet 1.",
-      link: "#"
+      description: "Développement d'un site pour un coder.",
+      image: coder,
+      link: "#",
+      createdWith: "PHP - SYMPFONY",
     },
     {
       title: "Projet 2",
-      description: "Description du projet 2.",
-      link: "#"
+      description: "Création d'un espace bien-être.",
+      image: espaceBienEtre,
+      link: "#",
+      createdWith: "LARAVEL",
     },
     {
       title: "Projet 3",
-      description: "Description du projet 3.",
-      link: "#"
+      description: "Développement d'un site pour Fresh Food.",
+      image: freshFood,
+      link: "#",
+      createdWith: "PHP et MySQL",
     },
     {
       title: "Projet 4",
-      description: "Description du projet 4.",
-      link: "#"
+      description: "Conception d'un site pour un restaurant japonais.",
+      image: restaurantJaponais,
+      link: "#",
+      createdWith: "WordPress",
     },
     {
       title: "Projet 5",
-      description: "Description du projet 5.",
-      link: "#"
+      description: "Création d'un design interactif (écrans multiples).",
+      image: screen,
+      link: "#",
+      createdWith: "FIGMA",
     },
     {
       title: "Projet 6",
-      description: "Description du projet 6.",
-      link: "#"
-    }
+      description: "Optimisation SEO pour une entreprise.",
+      image: seo,
+      link: "#",
+      createdWith: "Outils SEO",
+    },
   ];
+
+  useEffect(() => {
+    const setEqualHeights = () => {
+      const cards = document.querySelectorAll(".project-card");
+      let maxHeight = 0;
+
+      cards.forEach((card) => {
+        card.style.height = "auto";
+        if (card.offsetHeight > maxHeight) {
+          maxHeight = card.offsetHeight;
+        }
+      });
+
+      cards.forEach((card) => {
+        card.style.height = `${maxHeight}px`;
+      });
+    };
+
+    setEqualHeights();
+    window.addEventListener("resize", setEqualHeights);
+    return () => window.removeEventListener("resize", setEqualHeights);
+  }, []);
 
   return (
     <div className="portfolio">
       <Header />
+      <div className="banner">
+        <img src={banner} alt="Bannière" className="banner-image" />
+        <h1 className="banner-title">Portfolio</h1>
+        <p>Voici quelques-unes de mes réalisations</p>
+        <div className="banner-bar"></div>
+      </div>
       <Container className="portfolio-container">
-        <h1 className="portfolio-title">Portfolio de John Doe</h1>
-        <Row>
-          {projects.map((project, index) => (
-            <Col xs={12} md={6} lg={4} key={index} className="d-flex align-items-stretch">
+        {/* Première ligne : Projets 1 à 3 */}
+        <Row className="mb-4">
+          {projects.slice(0, 3).map((project, index) => (
+            <Col
+              xs={12}
+              md={6}
+              lg={4}
+              key={index}
+              className="d-flex align-items-stretch"
+            >
               <Card className="project-card flex-fill">
+                <div className="position-relative">
+                  <img
+                    src={project.image}
+                    alt={project.title}
+                    className="img-fluid"
+                  />
+                </div>
                 <Card.Body>
                   <Card.Title>{project.title}</Card.Title>
                   <Card.Text>{project.description}</Card.Text>
@@ -55,6 +116,40 @@ const Portfolio = () => {
                     Voir le projet <FontAwesomeIcon icon={faExternalLinkAlt} />
                   </Button>
                 </Card.Body>
+                {/* Sous-cadre sans mention "Créé avec" */}
+                <div className="created-with">{project.createdWith}</div>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+
+        {/* Deuxième ligne : Projets 4 à 6 */}
+        <Row className="mb-5">
+          {projects.slice(3, 6).map((project, index) => (
+            <Col
+              xs={12}
+              md={6}
+              lg={4}
+              key={index}
+              className="d-flex align-items-stretch"
+            >
+              <Card className="project-card flex-fill">
+                <div className="position-relative">
+                  <img
+                    src={project.image}
+                    alt={project.title}
+                    className="img-fluid"
+                  />
+                </div>
+                <Card.Body>
+                  <Card.Title>{project.title}</Card.Title>
+                  <Card.Text>{project.description}</Card.Text>
+                  <Button variant="primary" href={project.link}>
+                    Voir le projet <FontAwesomeIcon icon={faExternalLinkAlt} />
+                  </Button>
+                </Card.Body>
+                {/* Sous-cadre sans mention "Créé avec" */}
+                <div className="created-with">{project.createdWith}</div>
               </Card>
             </Col>
           ))}
